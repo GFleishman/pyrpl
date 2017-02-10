48dd80508cc270bdac0b2d9c7362a4d6e8925a33
gfleishman <greg.nli10me@gmail.com>
2017-02-10 12:17:40 -0500

    trouble shooting pyrpl import statement

--- Accessories/compute_jd.py
+++ Accessories/compute_jd.py
@@ -8,19 +8,17 @@ Created on Wed Dec 16 13:00:38 2015
 import sys
 import numpy as np
 import nibabel as nib
-#import pyrpl.image_tools.vcalc as vcalc
+import pyrpl.image_tools.vcalc as vcalc
 
-print '\n'.join(sys.path)
+path = sys.argv[1]
+wPath = sys.argv[2]
+vox = np.array([1., 1., 1.])
 
-#path = sys.argv[1]
-#wPath = sys.argv[2]
-#vox = np.array([1., 1., 1.])
-#
-#uf1 = np.empty((220, 220, 220, 3))
-#for i in range(3):
-#    p = path + str(i) + '.nii.gz'
-#    uf1[..., i] = nib.load(p).get_data().squeeze()
-#
-#jd = np.linalg.det(vcalc.jacobian(uf1, vox))
-#jd = nib.Nifti1Image(jd, np.eye(4))
-#nib.save(jd, wPath)
+uf1 = np.empty((220, 220, 220, 3))
+for i in range(3):
+    p = path + str(i) + '.nii.gz'
+    uf1[..., i] = nib.load(p).get_data().squeeze()
+
+jd = np.linalg.det(vcalc.jacobian(uf1, vox))
+jd = nib.Nifti1Image(jd, np.eye(4))
+nib.save(jd, wPath)
