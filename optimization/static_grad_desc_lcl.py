57296cb2fbf387ec876043430773c464792be977
gfleishman <greg.nli10me@gmail.com>
2017-02-14 15:19:31 -0500

    trouble shoot index bug

--- optimization/static_grad_desc_lcl.py
+++ optimization/static_grad_desc_lcl.py
@@ -94,8 +94,13 @@ def display_feedback(P0_mag, data_match, k, gr):
     plt.colorbar()
     fig.add_subplot(4, gr.dc.N, 3*gr.dc.N + 1)
     for i in range(1, gr.dc.N):
-        temp1 = np.arange(k)
-        temp2 = data_match[i, :k]
+        print type(data_match)
+        print data_match.shape
+        print type(data_match[0])
+        print type(i)
+        print i
+        print type(k)
+        print k
         plt.plot(np.arange(k), data_match[i, :k])
     fig.add_subplot(4, gr.dc.N, 3*gr.dc.N + 2)
     plt.plot(np.arange(k), P0_mag[:k])
